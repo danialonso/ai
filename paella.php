7eaeff2f828718b7f13e4d4b632c542e6468534f
danialonso <dani@motosdeagua.es>
2018-06-07 09:50:34 +0200

    Add files via upload

---
+++ paella.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+    <title>Analyze Image Sample</title>
+</head>
+<body>
+<?php
+// Replace <Subscription Key> with a valid subscription key.
+$ocpApimSubscriptionKey = '566c6db74fb543308fb3c2c894ce5d7e';
+
+// You must use the same location in your REST call as you used to obtain
+// your subscription keys. For example, if you obtained your subscription keys
+// from westus, replace "westcentralus" in the URL below with "westus".
+$uriBase = 'https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/f1dfa550-4392-4133-b798-9b76de62da5d/url';
+
+$imageUrl = 'http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg';
+
+require_once 'HTTP/Request2.php';
+
+$request = new Http_Request2($uriBase . '/analyze');
+$url = $request->getUrl();
+
+$headers = array(
+    // Request headers
+    'Content-Type' => 'application/json',
+    'Ocp-Apim-Subscription-Key' => $ocpApimSubscriptionKey
+);
+$request->setHeader($headers);
+
+$parameters = array(
+    // Request parameters
+    'visualFeatures' => 'Categories,Description',
+    'details' => '',
+    'language' => 'en'
+);
+$url->setQueryVariables($parameters);
+
+$request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body parameters
+$body = json_encode(array('url' => $imageUrl));
+
+// Request body
+$request->setBody($body);
+
+try
+{
+    $response = $request->send();
+    echo "<pre>" .
+        json_encode(json_decode($response->getBody()), JSON_PRETTY_PRINT) . "</pre>";
+}
+catch (HttpException $ex)
+{
+    echo "<pre>" . $ex . "</pre>";
+}
+?>
+</body>
+</html>
